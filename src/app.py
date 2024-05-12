f2d42b4e9058625a07ce7d11061ef4176d81036a
agustin-kiryk <agustin.kiryk@gmail.com>
2024-05-12 09:10:47 -0300

    add dependencies

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from dotenv import load_dotenv
+from config.mongodb import mongo
+import os
+from routes.register import register_blueprint
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+mongo.init_app(app)
+
+
+@app.route('/')
+def index():
+ return 'Hello world'
+
+app.register_blueprint(register_blueprint, url_prefix = '/user')
+
+if __name__ == '__main__':
+ app.run(debug=True)
