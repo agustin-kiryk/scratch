7e54ed41aebebc2ef16c0e8f38fc29d514b25ef5
agustin-kiryk <agustin.kiryk@gmail.com>
2024-06-02 19:30:11 -0300

    wip

--- src/services/Registration_service.py
+++ src/services/Registration_service.py
@@ -1,8 +1,10 @@
 import json
+from datetime import datetime
+
 from flask import Response
 from src.client.Twilio_client import send_verification_sms, verify_sms
 from src.client.Flask_mail_client import send_verification_email, verify_email_code
-from src.client.Paycaddy_client import create_user
+from src.client.Paycaddy_client import create_user_paycaddy
 from src.enums.Kyc_enum import CardOrderKycStatus
 from src.models.card_order import CardOrder
 from src.models.User_model import User
@@ -12,11 +14,21 @@ from src.repositories.User_temp_repository import TempUserRepository
 import bcrypt
 import secrets
 from src.config.mongodb import mongo
+from pydantic import Field, ValidationError
+
 
 class RegistrationService:
 
     @staticmethod
     def handle_step_1(data):
+        required_fields = ['name', 'lastName', 'livePanama', 'birthDate', 'email', 'phone_number', 'password', 'nationality']
+
+        # Verificar que todos los campos requeridos están presentes en los datos enviados
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            response_data = {'error': f'Missing required fields: {", ".join(missing_fields)}'}
+            return Response(json.dumps(response_data), status=400, mimetype='application/json')
+
         user_email = data.get('email')
         user_repo = UserRepository()
         temp_user_repo = TempUserRepository()
@@ -29,13 +41,32 @@ class RegistrationService:
             return Response(json.dumps(response_data), status=409, mimetype='application/json')
 
         phone_number = data.get('phone_number')
-        send_verification_sms(phone_number)
 
-        temp_user = TempUser(
-            email=user_email,
-            phone_number=phone_number,
-            verification_code=secrets.token_hex(4)
-        )
+        sms_result = send_verification_sms(phone_number)
+        if isinstance(sms_result, dict) and 'error' in sms_result:
+            response_data = {
+                'error': 'Failed to send verification SMS.',
+                'detail': sms_result
+            }
+            return Response(json.dumps(response_data), status=400, mimetype='application/json')
+
+        hashed_password = RegistrationService.hash_password(data.get('password'))
+
+        try:
+            temp_user = TempUser(
+                name=data.get('name'),
+                lastName=data.get('lastName'),
+                livePanama=data.get('livePanama'),
+                birthDate=data.get('birthDate'),
+                email=user_email,
+                phoneNumber=phone_number,
+                password=hashed_password,
+                verification_code=secrets.token_hex(4),
+                nationality=data.get('nationality')
+            )
+        except ValidationError as e:
+            response_data = {'error': 'Invalid data', 'detail': e.errors()}
+            return Response(json.dumps(response_data), status=400, mimetype='application/json')
 
         temp_user_repo.insert(temp_user)
 
@@ -44,24 +75,24 @@ class RegistrationService:
 
     @staticmethod
     def handle_step_2(data):
-        phone_number = data.get('phone_number')
         sms_code = data.get('sms_code')
+        phone_number = data.get('phone_number')
         email = data.get('email')
 
         if not phone_number or not sms_code:
             response_data = {'error': 'Phone number and SMS code are required'}
             return Response(json.dumps(response_data), status=400, mimetype='application/json')
-
         temp_user_repo = TempUserRepository()
         temp_user = temp_user_repo.find_by_email(email)
 
         if temp_user:
-            if temp_user.phone_verified:
+            if temp_user.phoneVerified:
                 # Si el teléfono ya está verificado, solo envía el código de verificación por email
                 email_verification_code = RegistrationService.generate_pin()
                 try:
                     send_verification_email(email, email_verification_code)
                     temp_user.email_verification_code = email_verification_code
+                    temp_user.email_verification_code = 'chenge CODE XXXXXXXXXXXXXXXXXXXXXXXX'
                     temp_user_repo.update(temp_user)
                     response_data = {'message': 'Email verification code sent'}
                     return Response(json.dumps(response_data), status=200, mimetype='application/json')
@@ -72,12 +103,17 @@ class RegistrationService:
                 # Verifica el estado de la verificación del SMS
                 verification_status = verify_sms(phone_number, sms_code)
                 if verification_status == 'approved':
-                    temp_user.phone_verified = True
+                    temp_user.phoneVerified = True
+                    temp_user.nationality = 'change desc'
                     email_verification_code = RegistrationService.generate_pin()
                     try:
                         send_verification_email(email, email_verification_code)
                         temp_user.email_verification_code = email_verification_code
+                        print(f'Before update: {temp_user.phoneVerified}')  # Debugging line
                         temp_user_repo.update(temp_user)
+                        update_temp_user = temp_user_repo.find_by_email(email)
+                        print(f'After update: {update_temp_user.phoneVerified}')
+
                         response_data = {'message': 'SMS verified and email verification code sent'}
                         return Response(json.dumps(response_data), status=200, mimetype='application/json')
                     except Exception as e:
@@ -100,9 +136,11 @@ class RegistrationService:
 
         temp_user = temp_user_repo.find_by_email(user_email)
 
-        if temp_user and verify_email_code(email_code, temp_user.email_verification_code) and temp_user.phone_verified:
-            hashed_password = RegistrationService.hash_password(temp_user.password)
-            new_user = RegistrationService.build_new_user(temp_user, hashed_password)
+        if temp_user and verify_email_code(email_code, temp_user.email_verification_code) and temp_user.phoneVerified:
+            temp_user.email_verified = True
+            temp_user_repo.update(temp_user)
+
+            new_user = RegistrationService.build_new_user(temp_user, temp_user.password)
 
             result = user_repo.insert(new_user)
 
@@ -125,7 +163,7 @@ class RegistrationService:
             response_data = {'error': 'Missing required fields'}
             return Response(json.dumps(response_data), status=400, mimetype='application/json')
 
-        user_response = create_user(data)
+        user_response = create_user_paycaddy(data)
 
         if 'id' not in user_response:
             response_data = {"error": "Failed to create user in PayCaddy", 'detail': user_response.get('title')}
@@ -159,11 +197,17 @@ class RegistrationService:
         new_user = User(
             name=temp_user.name,
             lastName=temp_user.lastName,
-            document=temp_user.document,
+            nationality=temp_user.nationality,
+            birtDate=temp_user.birthDate,
+            email_verified=temp_user.email_verified,
+            phoneNumber=temp_user.phoneNumber,
+            phoneVerified=temp_user.phoneVerified,
+            livePanama=temp_user.livePanama,
+            createAt=temp_user.created_at,
+            updateAt=Field(default_factory=datetime.utcnow),
             email=temp_user.email,
             points=0,
             status=1,
-            pin=None,
             password=hashed_password
         )
         return new_user
@@ -175,10 +219,11 @@ class RegistrationService:
             'id': user_id,
             'name': new_user.name,
             'lastName': new_user.lastName,
-            'document': new_user.document,
             'email': new_user.email,
             'points': new_user.points,
-            'status': new_user.status
+            'status': new_user.status,
+            'phoneVerified': new_user.phoneVerified,
+            'emailVerified': new_user.email_verified
         }
         return user_data
 
